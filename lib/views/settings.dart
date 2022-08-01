ded2a568be70b9791cb72e6165e0e4bccfa29f18
Matthew Krohn <gokloh@gmail.com>
2022-08-01 02:59:56 -0800

    added some settings

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:spanish_words/models/user.dart';
 
 class Settings extends StatefulWidget {
-  const Settings({Key? key}) : super(key: key);
+  const Settings({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  final User user;
 
   @override
   State<Settings> createState() => _SettingsState();
@@ -14,13 +20,87 @@ class _SettingsState extends State<Settings> {
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Generate New Set'),
-              ),
-            ],
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    widget.user.generateSet().then((_) => widget.user
+                        .saveToFirebase()
+                        .then((_) => setState(() {})));
+                  },
+                  child: const Text('Generate New Set'),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Increase Speech Attempts',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF006874),
+                        borderRadius: BorderRadius.circular(
+                          20,
+                        ),
+                      ),
+                      child: IconButton(
+                        onPressed: () async {
+                          if (widget.user.attempts > 1) {
+                            widget.user.attempts -= 1;
+                            await widget.user.saveToFirebase();
+                            setState(() {});
+                          }
+                        },
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      widget.user.attempts.toString(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF006874),
+                        borderRadius: BorderRadius.circular(
+                          20,
+                        ),
+                      ),
+                      child: IconButton(
+                        onPressed: () async {
+                          widget.user.attempts += 1;
+                          await widget.user.saveToFirebase();
+                          setState(() {});
+                        },
+                        icon: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
