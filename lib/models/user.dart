ba53dbfc6878029757da24aef2a4c3cf7e726476
Matthew Krohn <gokloh@gmail.com>
2022-08-01 02:41:25 -0800

    fixed small bug

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -56,7 +56,7 @@ class User with ChangeNotifier {
 
   Future<void> incrementMastery(Word word) async {
     Word userWord =
-        words.firstWhere((element) => element.english == word.english);
+        currentSet!.firstWhere((element) => element.english == word.english);
     if (userWord.mastery != null && userWord.mastery! < 1) {
       userWord.mastery = userWord.mastery! + 0.05;
     } else {
