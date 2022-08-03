b920195f3dc7a7a576082a9a887b312cc18d5341
Matthew Krohn <gokloh@gmail.com>
2022-08-03 02:40:57 -0800

    added master english/spanish toggle

--- lib/views/flashcards_view.dart
+++ lib/views/flashcards_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spanish_words/models/user.dart';
+import 'package:spanish_words/models/words.dart';
 import 'package:spanish_words/widgets/flashcard.dart';
 import 'package:spanish_words/widgets/white_text.dart';
 
@@ -17,19 +18,64 @@ class FlashcardsView extends StatefulWidget {
 
 class _FlashcardsViewState extends State<FlashcardsView> {
   int _index = 0;
-  bool showAnswer = false;
+  List<bool> isSelected = [true, false];
+  late List<FlashCard> _flashCards;
 
-  void updateShowAnswer() {
-    setState(() => showAnswer = !showAnswer);
+  List<FlashCard> getFlashcards() {
+    return List<FlashCard>.from(
+      widget.user.currentSet!.map((e) => FlashCard(
+            key: ValueKey<Word>(e),
+            word: e,
+            showAnswer: !isSelected[0],
+          )),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    _flashCards = getFlashcards();
     return SafeArea(
       child: Scaffold(
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            ToggleButtons(
+              onPressed: (index) {
+                setState(() {
+                  for (int buttonIndex = 0;
+                      buttonIndex < isSelected.length;
+                      buttonIndex++) {
+                    if (buttonIndex == index) {
+                      isSelected[buttonIndex] = true;
+                    } else {
+                      isSelected[buttonIndex] = false;
+                    }
+                  }
+                });
+              },
+              isSelected: isSelected,
+              fillColor: const Color(0xFF006874),
+              borderRadius: BorderRadius.circular(8.0),
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: WhiteText(
+                    'English',
+                    fontSize: 32,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: WhiteText(
+                    'Spanish',
+                    fontSize: 32,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -38,78 +84,57 @@ class _FlashcardsViewState extends State<FlashcardsView> {
                 ),
               ],
             ),
-            FlashCard(
-              word: widget.user.currentSet![_index],
-              showAnswer: showAnswer,
-              showAnswerCallback: updateShowAnswer,
-            ),
+            _flashCards[_index],
             const SizedBox(
               height: 15.0,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  onPressed: () async {
-                    widget.user.generateSet(null).then((_) => widget.user
-                        .saveToFirebase()
-                        .then((_) => setState(() {})));
-                  },
-                  child: const Text('Next Set'),
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF006874),
+                    borderRadius: BorderRadius.circular(
+                      20,
+                    ),
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      if (_index == 0) {
+                        setState(() => _index = 9);
+                      } else {
+                        setState(() => _index -= 1);
+                      }
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.3,
+                const SizedBox(
+                  width: 55,
                 ),
-                Row(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF006874),
-                        borderRadius: BorderRadius.circular(
-                          20,
-                        ),
-                      ),
-                      child: IconButton(
-                        onPressed: () {
-                          showAnswer = false;
-                          if (_index == 0) {
-                            setState(() => _index = 9);
-                          } else {
-                            setState(() => _index -= 1);
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.arrow_back,
-                          color: Colors.white,
-                        ),
-                      ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF006874),
+                    borderRadius: BorderRadius.circular(
+                      20,
                     ),
-                    const SizedBox(
-                      width: 10,
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      if (_index == 9) {
+                        setState(() => _index = 0);
+                      } else {
+                        setState(() => _index += 1);
+                      }
+                    },
+                    icon: const Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF006874),
-                        borderRadius: BorderRadius.circular(
-                          20,
-                        ),
-                      ),
-                      child: IconButton(
-                        onPressed: () {
-                          showAnswer = false;
-                          if (_index == 9) {
-                            setState(() => _index = 0);
-                          } else {
-                            setState(() => _index += 1);
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.arrow_forward,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ],
             ),
--- lib/widgets/flashcard.dart
+++ lib/widgets/flashcard.dart
@@ -8,12 +8,10 @@ class FlashCard extends StatefulWidget {
     Key? key,
     required this.word,
     required this.showAnswer,
-    required this.showAnswerCallback,
   }) : super(key: key);
 
   final Word word;
   final bool showAnswer;
-  final Function showAnswerCallback;
 
   @override
   State<FlashCard> createState() => _FlashCardState();
@@ -21,6 +19,12 @@ class FlashCard extends StatefulWidget {
 
 class _FlashCardState extends State<FlashCard> {
   FlutterTts flutterTts = FlutterTts();
+  bool? _override;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   Future _speak(String string) async {
     await flutterTts.setLanguage("es-MX");
@@ -31,7 +35,12 @@ class _FlashCardState extends State<FlashCard> {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => widget.showAnswerCallback(),
+      onTap: () => setState(() => {
+            if (_override == null)
+              _override = !widget.showAnswer
+            else
+              _override = !_override!
+          }),
       child: SizedBox(
         width: MediaQuery.of(context).size.width * 0.9,
         height: MediaQuery.of(context).size.width * 0.4,
@@ -45,9 +54,13 @@ class _FlashCardState extends State<FlashCard> {
                   child: FittedBox(
                     fit: BoxFit.scaleDown,
                     child: WhiteText(
-                      !widget.showAnswer
-                          ? widget.word.english
-                          : widget.word.spanish,
+                      _override != null
+                          ? _override!
+                              ? widget.word.spanish
+                              : widget.word.english
+                          : !widget.showAnswer
+                              ? widget.word.english
+                              : widget.word.spanish,
                       fontSize: 100,
                     ),
                   ),
