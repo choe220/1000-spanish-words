52fa90cc6cbdebdbe8e89fcdb663d7b611fdf197
Matthew Krohn <gokloh@gmail.com>
2022-08-06 12:31:20 -0800

    made flashcards scrollable

--- lib/views/flashcards_view.dart
+++ lib/views/flashcards_view.dart
@@ -16,10 +16,13 @@ class FlashcardsView extends StatefulWidget {
   State<FlashcardsView> createState() => _FlashcardsViewState();
 }
 
-class _FlashcardsViewState extends State<FlashcardsView> {
+class _FlashcardsViewState extends State<FlashcardsView>
+    with SingleTickerProviderStateMixin {
   int _index = 0;
   List<bool> isSelected = [true, false];
   late List<FlashCard> _flashCards;
+  PageController _flashCardPageController =
+      PageController(viewportFraction: 0.8);
 
   List<FlashCard> getFlashcards() {
     return List<FlashCard>.from(
@@ -37,107 +40,127 @@ class _FlashcardsViewState extends State<FlashcardsView> {
     return SafeArea(
       child: Scaffold(
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            ToggleButtons(
-              onPressed: (index) {
-                setState(() {
-                  for (int buttonIndex = 0;
-                      buttonIndex < isSelected.length;
-                      buttonIndex++) {
-                    if (buttonIndex == index) {
-                      isSelected[buttonIndex] = true;
-                    } else {
-                      isSelected[buttonIndex] = false;
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: ToggleButtons(
+                onPressed: (index) {
+                  setState(() {
+                    for (int buttonIndex = 0;
+                        buttonIndex < isSelected.length;
+                        buttonIndex++) {
+                      if (buttonIndex == index) {
+                        isSelected[buttonIndex] = true;
+                      } else {
+                        isSelected[buttonIndex] = false;
+                      }
                     }
-                  }
-                });
-              },
-              isSelected: isSelected,
-              fillColor: const Color(0xFF006874),
-              borderRadius: BorderRadius.circular(8.0),
-              children: const [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: WhiteText(
-                    'English',
-                    fontSize: 32,
+                  });
+                },
+                isSelected: isSelected,
+                fillColor: const Color(0xFF006874),
+                borderRadius: BorderRadius.circular(8.0),
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: WhiteText(
+                      'English',
+                      fontSize: 32,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: WhiteText(
-                    'Spanish',
-                    fontSize: 32,
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: WhiteText(
+                      'Spanish',
+                      fontSize: 32,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Column(
               children: [
-                WhiteText(
-                  '${_index + 1} / 10',
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        WhiteText(
+                          '${_index + 1} / 10',
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 200.0,
+                      child: PageView.builder(
+                        controller: PageController(viewportFraction: 0.8),
+                        itemCount: _flashCards.length,
+                        onPageChanged: (int index) =>
+                            setState(() => _index = index),
+                        itemBuilder: (context, index) {
+                          return Transform.scale(
+                            scale: index == _index ? 1 : 0.9,
+                            child: _flashCards[index],
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: [
+                //     Container(
+                //       decoration: BoxDecoration(
+                //         color: const Color(0xFF006874),
+                //         borderRadius: BorderRadius.circular(
+                //           20,
+                //         ),
+                //       ),
+                //       child: IconButton(
+                //         onPressed: () {
+                //           if (_index == 0) {
+                //             setState(() => _index = 9);
+                //           } else {
+                //             setState(() => _index -= 1);
+                //           }
+                //         },
+                //         icon: const Icon(
+                //           Icons.arrow_back,
+                //           color: Colors.white,
+                //         ),
+                //       ),
+                //     ),
+                //     const SizedBox(
+                //       width: 55,
+                //     ),
+                //     Container(
+                //       decoration: BoxDecoration(
+                //         color: const Color(0xFF006874),
+                //         borderRadius: BorderRadius.circular(
+                //           20,
+                //         ),
+                //       ),
+                //       child: IconButton(
+                //         onPressed: () {
+                //           if (_index == 9) {
+                //             setState(() => _index = 0);
+                //           } else {
+                //             setState(() => _index += 1);
+                //           }
+                //         },
+                //         icon: const Icon(
+                //           Icons.arrow_forward,
+                //           color: Colors.white,
+                //         ),
+                //       ),
+                //     ),
+                //   ],
+                // ),
               ],
             ),
-            _flashCards[_index],
-            const SizedBox(
-              height: 15.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF006874),
-                    borderRadius: BorderRadius.circular(
-                      20,
-                    ),
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      if (_index == 0) {
-                        setState(() => _index = 9);
-                      } else {
-                        setState(() => _index -= 1);
-                      }
-                    },
-                    icon: const Icon(
-                      Icons.arrow_back,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 55,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF006874),
-                    borderRadius: BorderRadius.circular(
-                      20,
-                    ),
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      if (_index == 9) {
-                        setState(() => _index = 0);
-                      } else {
-                        setState(() => _index += 1);
-                      }
-                    },
-                    icon: const Icon(
-                      Icons.arrow_forward,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            Container(),
           ],
         ),
       ),
